8711bb177b751906b8dab0b42c4f6530973b6c4f
Ana Saftoiu <saftoiu.ana.23@gmail.com>
2023-01-20 01:44:38 +0200

    Ana-01-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">samp</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
